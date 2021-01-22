2253bfb9c713e1fa3efb5a2e4a95fc7a57b030d1
kksalim77 <77684053+kksalim77@users.noreply.github.com>
2021-01-22 15:42:28 +0530

    Add files via upload

---
+++ IIS_Install.ps1
@@ -0,0 +1,5 @@
+﻿$servers = Get-Content "C:\Data\servers.txt"
+foreach ($server in $servers)
+{
+Install-WindowsFeature -ConfigurationFilePath "<folderpath>\DeploymentConfigTemplate.xml" -ComputerName $server
+}
